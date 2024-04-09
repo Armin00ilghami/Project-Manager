095583639a8acba2d0a96f7bc88568d71f43318a
armin ilghami <arminilghami11@gmail.com>
2024-04-09 17:19:55 +0330

    files open code

--- app/src/main/java/com/example/projectmanager/Adapter/FileAdapter.kt
+++ app/src/main/java/com/example/projectmanager/Adapter/FileAdapter.kt
@@ -9,30 +9,43 @@ import com.example.projectmanager.databinding.ItemFileLinearBinding
 import java.io.File
 import java.net.URLConnection
 
-class FileAdapter (val data:ArrayList<File>):RecyclerView.Adapter<FileAdapter.FileViewHolder>(){
+class FileAdapter ( val data:ArrayList<File> , val fileEvent: FileEvent ):RecyclerView.Adapter<FileAdapter.FileViewHolder>(){
     lateinit var binding : ItemFileLinearBinding
 
     inner class FileViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
 
         fun bindViews(file:File){
+            var fileType = ""
             binding.textView.text = file.name
+
             if ( file.isDirectory ){
                 binding.imageView.setImageResource( R.drawable.ic_folder)
             } else{
-                //binding.imageView.setImageResource( R.drawable.ic_file )
+
                 when {
                     isImage( file.path ) -> {
                         binding.imageView.setImageResource( R.drawable.ic_image )
+                        fileType = "image/*"
                     }
                     isVideo( file.path ) -> {
                         binding.imageView.setImageResource( R.drawable.ic_video )
+                        fileType = "video/*"
                     }
                     isZip( file.path ) -> {
                         binding.imageView.setImageResource( R.drawable.ic_zip )
+                        fileType = "application/zip"
                     }
                     else -> {
                         binding.imageView.setImageResource( R.drawable.ic_file )
+                        fileType = "text/plain"
                     }
+                }
+            }
+            itemView.setOnClickListener {
+                if ( file.isDirectory ){
+                    fileEvent.onFolderClicked( file.path )
+                } else {
+                    fileEvent.onFileClicked( file , fileType )
                 }
             }
         }
@@ -62,6 +75,11 @@ class FileAdapter (val data:ArrayList<File>):RecyclerView.Adapter<FileAdapter.Fi
     }
     fun isZip(name: String): Boolean {
         return name.contains(".zip") || name.contains(".rar")
+    }
+
+    interface FileEvent{
+        fun onFileClicked( file : File , type : String )
+        fun onFolderClicked( path : String )
     }
 
 }
--- app/src/main/java/com/example/projectmanager/Fragment/FileFragment.kt
+++ app/src/main/java/com/example/projectmanager/Fragment/FileFragment.kt
@@ -1,16 +1,22 @@
 package com.example.projectmanager.Fragment
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.FileProvider
+import com.example.projectmanager.R
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.projectmanager.Adapter.FileAdapter
 import com.example.projectmanager.databinding.FragmentFileBinding
 import java.io.File
 
-class FileFragment (val path : String): Fragment() {
+class FileFragment (val path : String): Fragment() , FileAdapter.FileEvent {
    lateinit var binding : FragmentFileBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -18,6 +24,7 @@ class FileFragment (val path : String): Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -27,13 +34,14 @@ class FileFragment (val path : String): Fragment() {
 
             val listOfFiles = arrayListOf<File>()
             listOfFiles.addAll( ourFile.listFiles()!! )
+            listOfFiles.sort()
 
             if ( listOfFiles.size > 0 ) {
 
                 binding.recyclerMain.visibility = View.VISIBLE
                 binding.imgNoData.visibility = View.GONE
 
-                val myAdapter = FileAdapter( listOfFiles )
+                val myAdapter = FileAdapter( listOfFiles , this )
                 binding.recyclerMain.adapter = myAdapter
                 binding.recyclerMain.layoutManager = LinearLayoutManager( context )
 
@@ -45,6 +53,37 @@ class FileFragment (val path : String): Fragment() {
             }
 
         }
+
+    }
+
+    override fun onFileClicked(file: File, type: String) {
+
+        val intent = Intent( Intent.ACTION_VIEW )
+
+        if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ){
+
+            val fileProvider = FileProvider.getUriForFile(
+                requireContext(),
+                requireActivity().packageName + "provider" ,
+                file
+            )
+            intent.setDataAndType( fileProvider , type )
+
+        } else {
+            intent.setDataAndType( Uri.fromFile( file ), type )
+        }
+
+        intent.addFlags( Intent.FLAG_GRANT_READ_URI_PERMISSION )
+        startActivity( intent )
+
+    }
+
+    override fun onFolderClicked(path: String) {
+
+        val transaction = parentFragmentManager.beginTransaction()
+        transaction.replace( R.id.frame_main_container , FileFragment( path ) )
+        transaction.addToBackStack(null)
+        transaction.commit()
 
     }
 
